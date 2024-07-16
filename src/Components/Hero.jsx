b1f93a3dc42d3d7342be517e27a8173d01a95464
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-07-16 14:27:25 -0600

    added id to section div

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <div>
+    <div id='hero--section'>
       <h1>Hi, I'm Danielle</h1>
       <p>a <span>Frontend Developer</span> creating inclusive spaces by increasing accessibility, one website at a time</p>
       <a href="#projects--section" className='button--primary large'>Check out my projects</a>
