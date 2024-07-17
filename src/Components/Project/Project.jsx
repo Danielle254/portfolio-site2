f0c5f5b03dae5b435985825e0b1b4e21e48c690d
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-07-17 11:47:23 -0600

    style projects section by creating cards

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -1,18 +1,39 @@
 import React from 'react'
 import './project.css'
+import img1 from '../../Images/project1thumb.png'
+import img2 from '../../Images/project2thumb.png'
+import img3 from '../../Images/project3thumb.png'
 
-export default function Project() {
+export default function Project(props) {
+  const tags = props.tags.map(
+    tag => {
+      return (
+        
+        <p className='tag'>{tag}</p>        
+      
+      )
+    }
+  )
+  let displayImage;
+  if (props.id === 1) {
+    displayImage = img1;
+  } else if (props.id === 2) {
+    displayImage = img2;
+  } else {
+    displayImage = img3;
+  }
+  
   return (
-    <div>
-      <img src=''  />
-      <h3>Project 1</h3>
-      <p>project summary here</p>
-      <div className='tags--container'>
-        <p className='tag'>React</p>
-        <p className='tag'>CSS</p>
-        <p className='tag'>Local Storage</p>
+    <div className='project--card'>
+      <img src={displayImage}  />
+      <div id='details--container'>
+        <h3>{props.title}</h3>
+        <p>{props.summary}</p>
+        <div className='tags--container'>
+          {tags}
+        </div>
+        <a href=''>Demo & Details</a>
       </div>
-      <button className='project--button'>Demo & Details</button>
     </div>
   )
 }
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,15 +1,30 @@
 import React from 'react'
 import Project from '../Project/Project'
 import './projects.css'
+import data from '../../projectData'
 
 export default function Projects() {
+  const projects = data.projectSummaries.map(
+    project => {
+      return (
+        <Project
+        key={project.id}
+        id={project.id}
+        title={project.title}
+        image={project.imageSource}
+        summary={project.summary}
+        tags={project.tags}
+        path={project.detailsPath}
+        />
+      )
+    }
+  )
+  
   return (
     <div id='projects--section'>
       <h2>Projects</h2>
-      <Project />
-      <Project />
-      <Project />
-      <a className='button--secondary large' href="https://github.com/Danielle254" target="_blank">More projects on GitHub</a>
+      {projects}
+      <a href="https://github.com/Danielle254" target="_blank">More projects on GitHub</a>
     </div>
   )
 }
