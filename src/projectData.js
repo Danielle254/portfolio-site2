f430638047d7ba5f2400e02f768bd6ffcc65f48a
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-07-17 11:47:07 -0600

    create object containing project data

---
+++ src/projectData.js
@@ -0,0 +1,26 @@
+export default {
+    projectSummaries: [
+        {
+            id: 1,
+            title: "Custom Conference Itinerary",    
+            summary: "description of project 1", 
+            tags: ["React", "Local Storage"],
+            detailsPath: "/project1"
+        }, 
+        {
+            id: 2,
+            title: "Dog Food Calculator",
+            summary: "tdescription of project 2", 
+            tags: ["JavaScript", "Bootstrap", "API"],
+            detailsPath: "/project2"
+        },
+        {
+            id: 3,
+            title: "Alpine Vet Redesign",
+            summary: "description of project 3", 
+            tags: ["CSS", "Accessibility"],
+            detailsPath: "/project3"
+        }
+    ],
+    projectDetails: []
+}
