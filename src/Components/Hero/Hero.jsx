956c323c1931fbb05470b662b502edbabace298d
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-07-17 08:44:39 -0600

    style hero section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ export default function Hero() {
     <div id='hero--section'>
       <h1>Hi, I'm Danielle</h1>
       <p>a <span>Frontend Developer</span> creating inclusive spaces by increasing accessibility, one website at a time</p>
-      <a href="#projects--section" className='button--primary large'>Check out my projects</a>
+      <a href="#projects--section" id='hero--button'>Check out my projects</a>
     </div>
   )
 }
