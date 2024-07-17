858baf8f6a1b2fc8c34740aad397214ee9940d4e
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-07-17 08:41:22 -0600

    set navbar content to a specific max width and set background gradient to be full screen

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,11 +6,13 @@ import './navbar.css'
 export default function Navbar(props) {
   
   return (
-    <div id='navbar--section'>
-      <img src={profile} />
-      <p>Danielle Lindblom</p>
-      <button onClick={props.toggleMenu}>MENU</button>
-      {props.menuVisible && <Menu />}
+    <div id='navbar--background'>
+      <div id='navbar--content'>
+        <img src={profile} />
+        <p>Danielle Lindblom</p>
+        <button onClick={props.toggleMenu}>MENU</button>
+        {props.menuVisible && <Menu />}
+      </div>
     </div>
   )
 }
