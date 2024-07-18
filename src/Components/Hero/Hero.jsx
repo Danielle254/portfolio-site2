9ccf38826ebe499538f828bccddf7a540bbfe350
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-07-18 16:42:05 -0600

    dark mode

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import './hero.css'
 
-export default function Hero() {
+export default function Hero(props) {
   return (
     <div id='hero--section'>
       <div id='hero--container'>
         <h1>Hi, I'm Danielle</h1>
-        <p>a <span>Frontend Developer</span> creating inclusive spaces by increasing accessibility, one website at a time</p>
-        <a href="#projects--section" id='hero--button'>View Projects</a>
+        <p>a <span id={props.mode === "light" ? 'span-light' : 'span-dark'}>Frontend Developer</span> creating inclusive spaces by increasing accessibility, one website at a time</p>
+        <a href="#projects--section" id={props.mode === "light" ? 'hero--button' : 'hero--button-dark'}>View Projects</a>
       </div>
     </div>
   )
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ export default function Navbar(props) {
   return (
     <div className={props.mode === "light" ? 'navbar--background' : 'navbar--background-dark'}>
       <div className='navbar--content'>
-        <img id='profile' src={profile} />
+        <img id={props.mode === "light" ? 'profile' : 'profile-dark'} src={profile} />
         <p className='nav--title'>Danielle Lindblom</p>
         <div className='menu--items'>
           <ul className='main--menu'>
@@ -19,7 +19,7 @@ export default function Navbar(props) {
             <li><a className={props.mode === "light" ? 'menu-light' : 'menu-dark'} href="">Resume</a></li>
           </ul>                       
           <button id={props.mode === "light" ? 'mode--button-desktop' : 'mode--button-desktop-dark'} className='access--button-desktop' onClick={props.toggleMode}><img className='icon' src={modeIcon} />{props.mode === "light" ? " Dark Mode" : " Light Mode"}</button>
-          <button id='animate--button-desktop' className='access--button-desktop' onClick={props.toggleAnimations}><img src={motionIcon} className='icon'/>{props.animationsOn === true ? "Animations OFF" : "Animations ON"}</button>           
+          <button id={props.mode === "light" ? 'animate--button-desktop' : 'animate--button-desktop-dark'} className='access--button-desktop' onClick={props.toggleAnimations}><img src={motionIcon} className='icon'/>{props.animationsOn === true ? "Animations OFF" : "Animations ON"}</button>           
         </div> 
         {props.mobileMenuVisible === false && <button id='menu--button' onClick={props.toggleMenu}>MENU</button>}         
       </div>
