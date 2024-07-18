1b4e14aa062f36c444121119fef7dcbe853eee69
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-07-18 15:34:07 -0600

    add copy to projects

--- src/projectData.js
+++ src/projectData.js
@@ -3,21 +3,21 @@ export default {
         {
             id: 1,
             title: "Custom Conference Itinerary",    
-            summary: "description of project 1", 
-            tags: ["React", "Local Storage"],
+            summary: "This website displays the agenda for a conference and allows the user to add activities from the official agenda to their own personal itinerary, which they can print as a nicely formatted document. If they need to make changes later, the browser stores their itinerary for them.", 
+            tags: ["React", "CSS", "Local Storage"],
             detailsPath: "/project1"
         }, 
         {
             id: 2,
             title: "Dog Food Calculator",
-            summary: "tdescription of project 2", 
+            summary: "This app calculates how long a given bag of dog food will last, factoring in a multiple dog household, and provides the user with pricing information and a link to purchase the food online. There is also the ability to compare 2 different foods.", 
             tags: ["JavaScript", "Bootstrap", "API"],
             detailsPath: "/project2"
         },
         {
             id: 3,
             title: "Alpine Vet Redesign",
-            summary: "description of project 3", 
+            summary: "I took the website of a local business that I frequent, Alpine Vet Care in Missoula, and gave the homepage a refresh. This project showcases my ability to use basic HTML and CSS to create a responsive, more accessible webpage for desktop and mobile.", 
             tags: ["CSS", "Accessibility"],
             detailsPath: "/project3"
         }
