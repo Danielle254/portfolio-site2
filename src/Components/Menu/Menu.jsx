ebdb2bd8598b92002337ccd9d1627ef018153fc2
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-07-18 14:09:08 -0600

    auto close mobile menu when menu item is clicked

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import './menu.css'
+import modeIcon from '../../Images/light-dark-mode-icon.png';
+import animateIcon from '../../Images/motion-icon.png';
 
 export default function Menu(props) {
   return (
     <nav id='nav'>
       <button id='close--nav' onClick={props.toggleMenu}>X</button>
       <ul>
-        <li><a href="#about--section">About</a></li>
-        <li><a href="#projects--section">Projects</a></li>
-        <li><a href="#contact--section">Contact</a></li>
-        <li><a href="">Resume</a></li>
+        <li><a href="#about--section" onClick={props.toggleMenu}>About</a></li>
+        <li><a href="#projects--section" onClick={props.toggleMenu}>Projects</a></li>
+        <li><a href="#contact--section" onClick={props.toggleMenu}>Contact</a></li>
+        <li><a href="" onClick={props.toggleMenu}>Resume</a></li>
         <div className='access--container'>
-          <button id='mode--button' className='access--button' onClick={props.toggleMode}>{props.mode === "light" ? "Dark Mode" : "Light Mode"}</button>
-          <button id='animate--button' className='access--button' onClick={props.toggleAnimations}>{props.animationsOn === true ? "Animations OFF" : "Animations ON"}</button>
+          <button id='mode--button' className='access--button' onClick={props.toggleMode}><img className='icon' src={modeIcon} />{props.mode === "light" ? "  Dark Mode" : "   Light Mode"}</button>
+          <button id='animate--button' className='access--button' onClick={props.toggleAnimations}><img src={animateIcon} className='icon'/>{props.animationsOn === true ? "Animations OFF" : "Animations ON"}</button>
         </div>
       </ul>
     </nav>
