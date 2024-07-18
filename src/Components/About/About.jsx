5c2f5dcbd565bcaeb1cc3387c728508bbee3aae5
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-07-18 15:11:53 -0600

    add copy to about section

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -5,7 +5,11 @@ export default function About() {
   return (
     <div id='about--section'>
       <h2>About Me</h2>
-      <p>add copy here add copy here add copy here add copy here add copy here add copy here add copy hereadd copy here add copy here add copy here add copy here add copy here add copy here add copy here add copy here add copy here add copy here add copy here add copy hereadd copy hereadd copy here add copy here add copy here add copy here add copy here add copy hereadd copy hereadd copy here add copy here add copy hereadd copy hereadd copy here</p>
+      <h4>Once an engineer, always an engineer.</h4>
+      <p>With a background in mechanical engineering, small business ownership, and technical training, I've always been a problem solver. Now, I'm bringing all of that experience to frontend web development.<br/><br/>
+      I built websites for my small businesses for many years using Wix. I'm fascinated with why people do what they do, and that includes using a website.<br/><br/>
+      I'm passionate about accessibility and inclusion, and I'm on a mission to make digital spaces more welcoming and easy to enjoy.<br/>
+      </p>
       <a href="" >Full Resume</a>
     </div>
   )
