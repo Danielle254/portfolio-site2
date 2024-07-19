9d4d4383cf0ce18c06edc790750aa4cc5710ae24
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-07-19 10:13:58 -0600

    add hover effect to "close mobile menu" X button

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import './menu.css'
 import modeIcon from '../../Images/light-dark.png';
-import modeIconWhite from '../../Images/light-dark-white.png'
 import animateIcon from '../../Images/motion-icon.png';
-import animateIconWhite from '../../Images/motion-icon-white.png'
+
 
 export default function Menu(props) {
   return (
@@ -15,7 +14,7 @@ export default function Menu(props) {
         <li><a href="#contact--section" onClick={props.toggleMenu}>Contact</a></li>
         <li><a href="" onClick={props.toggleMenu}>Resume</a></li>
         <div className='access--container'>
-          <button id={props.mode === "light" ? 'mode--button' : 'mode--button-dark'} className='access--button' onClick={props.toggleMode}><img className='icon' src={props.mode === "light" ? modeIcon : modeIconWhite} />{props.mode === "light" ? "  Dark Mode" : "   Light Mode"}</button>
+          <button id='mode--button' className='access--button' onClick={props.toggleMode}><img className='icon' src={modeIcon} />{props.mode === "light" ? "  Dark Mode" : "   Light Mode"}</button>
           <button id='animate--button' className='access--button' onClick={props.toggleAnimations}><img src={animateIcon} className='icon'/>{props.animationsOn === true ? "Animations OFF" : "Animations ON"}</button>
         </div>
       </ul>
