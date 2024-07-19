ae8c1c3bd07b6137675105b18e5975ff487b8fb1
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-07-19 16:37:08 -0600

    first breakpoint - 500px, adjusted spacing

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -5,27 +5,30 @@ export default function Skills(props) {
   return (
     <div id='skills--section'>
       <h2>Skills</h2>
-      <h4 className={props.mode === "light" ? 'h4-light' : 'h4-dark'}>Technical</h4>
-      <div id='list--container'>
-      <ul>
-        <li>React</li>
-        <li>JavaScript</li>
-        <li>HTML/CSS</li>
-        <li>Web Accessibility</li>
-      </ul>
-      <ul id='second-list'> 
-        <li>Sass, Bootstrap</li>
-        <li>Git & GitHub</li>
-        <li>Testing</li>
-        <li>VSCode</li>
-      </ul></div>
-      <h4 className={props.mode === "light" ? 'h4-light skills2' : 'h4-dark skills2'}>Professional</h4>
-      <ul>
-        <li>Team Leadership</li>
-        <li>Continuous Improvement</li>
-        <li>Technical Training</li>
-        <li>Project Collaboration</li>
-      </ul>
+      <div className='content--container'>
+        <h4 className={props.mode === "light" ? 'h4-light' : 'h4-dark'}>Technical</h4>
+        <div id='list--container'>
+          <ul>
+            <li>React</li>
+            <li>JavaScript</li>
+            <li>HTML/CSS</li>
+            <li>Web Accessibility</li>
+          </ul>
+          <ul id='second-list'> 
+            <li>Sass, Bootstrap</li>
+            <li>Git & GitHub</li>
+            <li>Testing</li>
+            <li>VSCode</li>
+          </ul>
+        </div>
+        <h4 className={props.mode === "light" ? 'h4-light skills2' : 'h4-dark skills2'}>Professional</h4>
+        <ul>
+          <li>Team Leadership</li>
+          <li>Continuous Improvement</li>
+          <li>Technical Training</li>
+          <li>Project Collaboration</li>
+        </ul>
+      </div>
     </div>
   )
 }
