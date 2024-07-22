6914199333256bcb8e8e878d930958586680bb0e
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-07-22 09:08:45 -0600

    routes established

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,46 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Project1 from './Pages/Project1.jsx'
+import Project2 from './Pages/Project2.jsx'
+import Project3 from './Pages/Project3.jsx'
+
+
+const Layout = () => {
+  return (
+    <div>      
+      <Outlet />      
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+  path: '/',
+  element: <Layout />,
+  children: [
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: '/project1',
+    element: <Project1 />
+  },
+  {
+    path: '/project2',
+    element: <Project2 />
+  },
+  {
+    path: '/project3',
+    element: <Project3 />
+  }
+],
+},
+]);
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>    
-    <App />    
-  </React.StrictMode>,
+  <RouterProvider router={router} />,
 )
