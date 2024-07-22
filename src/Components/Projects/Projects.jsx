284bb5d0980c68bc8db717a3c6fe7273bdea300d
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-07-22 09:20:24 -0600

    move project details data from separate file into Projects component file

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import Project from '../Project/Project'
 import './projects.css'
-import data from '../../projectData'
+
 
 export default function Projects(props) {
-  const projects = data.projectSummaries.map(
+  const projectData = [{
+    id: 1,
+    title: "Custom Conference Itinerary",    
+    summary: "This website displays the agenda for a conference and allows the user to add activities from the official agenda to their own personal itinerary, which they can print as a nicely formatted document. If they need to make changes later, the browser stores their itinerary for them.", 
+    tags: ["React", "CSS", "Local Storage"]
+    }, 
+    {
+    id: 2,
+    title: "Dog Food Calculator",
+    summary: "This app calculates how long a given bag of dog food will last, factoring in a multiple dog household, and provides the user with pricing information and a link to purchase the food online. There is also the ability to compare 2 different foods.", 
+    tags: ["JavaScript", "Bootstrap", "API"]
+    },
+    {
+    id: 3,
+    title: "Alpine Vet Redesign",
+    summary: "I took the website of a local business that I frequent, Alpine Vet Care in Missoula, and gave the homepage a refresh. This project showcases my ability to use basic HTML and CSS to create a responsive, more accessible webpage for desktop and mobile.", 
+    tags: ["CSS", "Accessibility"]
+    }];
+
+  const projects = projectData.map(
     project => {
       return (
         <Project
