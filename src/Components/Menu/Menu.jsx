24cbe5b1baa3de6278161bda299c84dbefbf72bc
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-07-22 15:42:06 -0600

    changed former a links to p elements due to react router Link element being added (clear error message)

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -6,14 +6,15 @@ import animateIcon from '../../Images/motion-icon.png';
 
 
 export default function Menu(props) {
+
   return (
     <nav id={props.lightMode ? 'nav' : 'nav-dark'}>
       <button id={props.lightMode ? 'close--nav' : 'close--nav-dark'} onClick={props.menuOff}>X</button>
       <ul>
-        <li><Link to={'/#about--section'} style={{'text-decoration': 'none'}}><a onClick={props.mobileReset} >About</a></Link></li>
-        <li><Link to={'/#projects--section'} style={{'text-decoration': 'none'}}><a onClick={props.mobileReset}  >Projects</a></Link></li>
-        <li><Link to={'/#contact--section'} style={{'text-decoration': 'none'}}><a onClick={props.mobileReset}  >Contact</a></Link></li>
-        <li><a onClick={props.mobileReset}  href="" >Resume</a></li>
+        <li><Link to={'/#about--section'} className={props.lightMode ? 'menu-link' : 'menu-link-dark'}><p onClick={props.mobileReset} >About</p></Link></li>
+        <li><Link to={'/#projects--section'}  className={props.lightMode ? 'menu-link' : 'menu-link-dark'}><p onClick={props.mobileReset}  >Projects</p></Link></li>
+        <li><Link to={'/#contact--section'}  className={props.lightMode ? 'menu-link' : 'menu-link-dark'}><p onClick={props.mobileReset}>Contact</p></Link></li>
+        <li><a onClick={props.mobileReset} href="" >Resume</a></li>
         <div className='access--container'>
           <button id={props.lightMode ? 'mode--button' : 'mode--button-dark'} className='access--button' onClick={props.toggleLightMode}><img className='icon' src={modeIcon} />{props.lightMode ? "  Dark Mode" : "   Light Mode"}</button>
           <button id={props.lightMode ? 'animate--button' : 'animate--button-dark'} className='access--button' onClick={props.toggleAnimations}><img src={animateIcon} className='icon'/>{props.animationsOn === true ? " Animations OFF" : " Animations ON"}</button>
