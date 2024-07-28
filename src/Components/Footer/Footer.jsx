861777ff051358e4f29d71319b68a28b42ec50ff
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-07-28 09:11:02 -0600

    add link to github repo for this project to footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import './footer.css'
 export default function Footer(props) {
   return (
     <footer className={props.lightMode ? 'footer' : 'footer-dark'}>
-      <p>Designed with Canva and built with React and Sass. <br/>© 2024 Danielle Lindblom</p>
+      <p>Designed with Canva and built with React and Sass. <a href="https://github.com/Danielle254/portfolio-site2">View Code</a><br/>© 2024 Danielle Lindblom</p>
     </footer>
   )
 }
