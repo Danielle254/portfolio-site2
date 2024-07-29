5e4679ab018391756155d81193a12fe3fddf580e
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-07-29 16:18:19 -0600

    first pass adding door graphic to hero section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import './hero.css'
+import door from '../../Images/door.png'
 
 export default function Hero(props) {
   return (
     <section id='hero--section'>
       <div id='hero--container'>
-        <h1>Hi, I'm Danielle</h1>
-        <p>a <span id={props.lightMode ? 'span-light' : 'span-dark'}>Frontend Developer</span> creating inclusive spaces by increasing accessibility, one website at a time</p>
-        <a href="/portfolio-site2/#projects--section" id={props.lightMode ? 'hero--button' : 'hero--button-dark'}>View Projects</a>
+        <div id='hero--content'>
+          <h1>Hi, I'm Danielle</h1>
+          <p>a <span id={props.lightMode ? 'span-light' : 'span-dark'}>Frontend Developer</span> creating inclusive spaces by increasing accessibility, one website at a time</p>
+          <a href="/portfolio-site2/#projects--section" id={props.lightMode ? 'hero--button' : 'hero--button-dark'}>View Projects</a>
+        </div>
+        {props.lightMode && <img src={door} id='door-image' alt='line drawing of a door opening' />}
       </div>
     </section>
   )
