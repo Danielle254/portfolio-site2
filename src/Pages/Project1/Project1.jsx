b61bb6790144a25417b9b6656ff839895299d5f3
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-08-29 14:24:12 -0600

    update project copy

--- src/Pages/Project1/Project1.jsx
+++ src/Pages/Project1/Project1.jsx
@@ -24,7 +24,7 @@ const Project1 = () => {
           </div>
           </div>
           <p>This is what I'd call my capstone project. It is broken down into multiple versions and deployments, and will ultimately be a multi-user, public application utilizing a database. I want to work outside of my comfort zone and create something that makes me grow my skills.<br/><br/>
-          <a href='https://docs.google.com/document/d/1qis5J6LwGeG49rQRjwhjirpdhUqHIUrBGo0ejoxNWIA/edit?usp=sharing'>View the full planning document</a>
+          <a target='_blank' className={lightMode ? 'link--light' : 'link--dark'} href='https://docs.google.com/document/d/1qis5J6LwGeG49rQRjwhjirpdhUqHIUrBGo0ejoxNWIA/edit?usp=sharing'>View the full planning document</a>
           <br/><br/>
           The idea for this project came from me, as a service dog handler, not being able to remember which businesses (particularly restuarants) I've visited were a good experience. Version 1 of this application allows users to capture information about the business and add a personal note regarding the comfort and safety of their service dog when visiting.
           <br/><br/>
@@ -38,27 +38,22 @@ const Project1 = () => {
             <li>Note which businesses were your favorites</li>
             <li>Sort entries by Date Visited or Business Name. Filter entries by Favorites.</li>
           </ul>
-          <h2>Skills Showcased</h2> {/* left off editing copy here */}
+          <h2>Skills Showcased</h2> 
           <ul className='bullets'>
-            <li>React components</li>
-            <li>Use of state, props, hooks, and event handlers</li>
+            <li>React components</li>            
             <li>Utilizing local storage</li>
-            <li>Sort function that uses the Date object</li>
-            <li>Passing information from parent to child and reverse</li>
-            <li>Responsive website</li>
-            <li>Flex and Grid</li>
+            <li>CRUD app - Create, Read, Update, Delete</li>
+            <li>Keyboard Navigable</li>
+            <li>Form Validation</li>            
           </ul>
-          <h2>Lessons Learned</h2>
-          <p>This project really required me to be organized with my file structure, which in turn kept me organized in the execution. <br/><br/>
-          I liked learning about local storage and the print function. I can see these being useful in many other applications. <br/><br/>
-          I appreciated that my understanding of how to pass functions and attributes to children components really came together through this project. <br/><br/>
-          I stepped away from this project for a few months, and when I returned, I was able to read through and understand the code base and how it was functioning. I think this is largely due to having clean, readable code that is well-labeled.</p>
+          
           <h2>Future Enhancement Ideas</h2>
-          <p>I'm actually planning another project like this where I create the conference agenda with some sort of library to display it visually in a more calendar-like format. This will make it easier to tell which talks happen at the same time, and even visually distinguish the locations of the talks. <br/><br/>
-          I'd also like to add a "share" button in addition to printing. <br/><br/>
-          Another idea would be to add "add to calendar" options so users could put the talks directly onto their own personal calendar (google, etc).</p>
-          <a className={lightMode ? 'repo' : 'repo-dark'} id='top--button' href='https://github.com/Danielle254/ServiceDogsAroundTown_v1'>GitHub Repo</a>
-          <a className={lightMode ? 'repo' : 'repo-dark'} href='https://danielle254.github.io/ServiceDogsAroundTown_v1/'>Live Link</a>
+          <p>Version 2 will incorporate the Google Maps API and utilize PocketBase for a database. There are further iterations/deployments scheduled after that as well. These first 2 versions will be single user, "private" applications. After that, it will open up to be a multi-user, "public" application that displays data from all users (except personal notes, which will be kept private to each user).
+          <br/><br/>
+          You can see the full planning document here: <a target='_blank' className={lightMode ? 'link--light' : 'link--dark'} href='https://docs.google.com/document/d/1qis5J6LwGeG49rQRjwhjirpdhUqHIUrBGo0ejoxNWIA/edit?usp=sharing'>View Next Steps for this project</a>
+          </p>
+          <a className={lightMode ? 'repo' : 'repo-dark'} id='top--button' href='https://github.com/Danielle254/ServiceDogsAroundTown_v1' target='_blank'>GitHub Repo</a>
+          <a className={lightMode ? 'repo' : 'repo-dark'} href='https://danielle254.github.io/ServiceDogsAroundTown_v1/' target='_blank'>Live Link</a>
           <Link to={'/project2'} className={lightMode ? 'next-project' : 'next-project-dark'} ><p>Next Project ➤</p></Link>
         </div>
       </main>
