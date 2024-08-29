7d59ab172305e9b88f7668b89e066e9086df2018
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-08-29 12:34:15 -0600

    adjust project list to add SDAT

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -4,6 +4,7 @@ import './project.css'
 import img1 from '../../Images/project1thumb.png'
 import img2 from '../../Images/project2thumb.png'
 import img3 from '../../Images/project3thumb.png'
+import img4 from "../../Images/project4thumb.png"
 import { v4 as uuid  } from "uuid"
 
 export default function Project(props) {
@@ -17,19 +18,29 @@ export default function Project(props) {
 
   let displayImage;
   let path = '';
-  if (props.id === 1) {
-    displayImage = img1;
-    path = '/project1';
-  } else if (props.id === 2) {
-    displayImage = img2;
-    path = '/project2';
-  } else {
-    displayImage = img3;
-    path = '/project3';
+  let cardClass = 'project--card';
+  switch (props.id) {
+    case 1:
+      displayImage = img1;
+      path = '/project1';
+      cardClass += ' feature';
+      break;
+    case 2:
+      displayImage = img2;
+      path = '/project2';
+      break;
+    case 3:
+      displayImage = img3;
+      path = '/project3';
+      break;
+    case 4:
+      displayImage = img4;
+      path = '/project4';
+      break;
   }
   
   return (
-    <div className='project--card'>
+    <div className={cardClass}>
       <Link to={path} >
       <img src={displayImage} className={props.lightMode ? 'img-light' : 'img-dark'} alt='screenshot of project website'/>
       </Link>
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -4,24 +4,24 @@ import './projects.css'
 
 
 export default function Projects(props) {
-  const projectData = [{
-    id: 1,
+  const projectData = [
+    {id: 1,
+    title: "Service Dogs Around Town",
+    summary: "This is Version 1 of my capstone project, aimed at helping service dog handlers record information about the businesses they frequent as it relates to the comfort and safety of their dogs.",
+    tags: ["React", "Sass", "Local Storage", "Forms"]},
+    {id: 2,
     title: "Custom Conference Itinerary",    
     summary: "This website displays the agenda for a conference and allows the user to add activities from the official agenda to their own personal itinerary, which they can print as a nicely formatted document. If they need to make changes later, the browser stores their itinerary for them.", 
-    tags: ["React", "CSS", "Local Storage"]
-    }, 
-    {
-    id: 2,
+    tags: ["React", "CSS", "Local Storage", "Format PDF"]}, 
+    {id: 3,
     title: "Dog Food Calculator",
     summary: "This app calculates how long a given bag of dog food will last, factoring in a multiple dog household, and provides the user with pricing information and a link to purchase the food online. There is also the ability to compare 2 different foods.", 
-    tags: ["JavaScript", "Bootstrap", "API"]
-    },
-    {
-    id: 3,
+    tags: ["JavaScript", "Bootstrap", "API", "Forms"]},
+    {id: 4,
     title: "Alpine Vet Redesign",
     summary: "I took the website of a local business that I frequent, Alpine Vet Care in Missoula, and gave the homepage a refresh. This project showcases my ability to use basic HTML and CSS to create a responsive, more accessible webpage for desktop and mobile.", 
-    tags: ["CSS", "Accessibility"]
-    }];
+    tags: ["HTML", "CSS", "Accessibility"]}
+  ];
 
   const projects = projectData.map(
     project => {
