34b8b3765f9018f665671af297e5e3a8ae4dba0c
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-09-03 13:48:33 -0600

    add new window icon to Resume menu links for accessibility

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -4,6 +4,8 @@ import './menu.css'
 import modeIcon from '../../Images/light-dark.png';
 import animateIcon from '../../Images/motion-icon.png';
 import resume from '../../resume.pdf'
+import newWindow from '../../Images/external-link-icon.png'
+import newWindowWhite from '../../Images/external-link-icon-white.png'
 
 
 export default function Menu(props) {
@@ -24,7 +26,7 @@ export default function Menu(props) {
           <li><Link to={'/#about--section'} className={props.lightMode ? 'menu-link' : 'menu-link-dark'}  onClick={props.mobileReset}>About</Link></li>
           <li><Link to={'/#projects--section'}  className={props.lightMode ? 'menu-link' : 'menu-link-dark'}  onClick={props.mobileReset}>Projects</Link></li>
           <li><Link to={props.lightMode ? '/#contact--section' : '/#contact--section-dark'}  className={props.lightMode ? 'menu-link' : 'menu-link-dark'} onClick={props.mobileReset}>Contact</Link></li>
-          <li><a onClick={props.mobileReset} href={resume} target='_blank'>Resume</a></li>
+          <li><a onClick={props.mobileReset} href={resume} target='_blank'>Resume</a><img alt='' className='new-window-icon' src={props.lightMode ? newWindow : newWindowWhite} /></li>
           <div className='access--container'>
             <button id={props.lightMode ? 'mode--button' : 'mode--button-dark'} className='access--button' onClick={props.toggleLightMode}><img className='icon' src={modeIcon} alt='' />{props.lightMode ? "  Dark Mode" : "   Light Mode"}</button>
             <button id={props.lightMode ? 'animate--button' : 'animate--button-dark'} className='access--button' onClick={props.toggleAnimations}><img src={animateIcon} className='icon' alt=''/>{props.animationsOn === true ? " Animations OFF" : " Animations ON"}</button>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,6 +7,8 @@ import motionIcon from '../../Images/motion-icon.png';
 import './navbar.css'
 import motionIconWhite from '../../Images/motion-icon-white.png'
 import resume from '../../resume.pdf'
+import newWindow2 from '../../Images/external-link-icon.png'
+import newWindowWhite2 from '../../Images/external-link-icon-white.png'
 
 export default function Navbar(props) {
   
@@ -22,7 +24,7 @@ export default function Navbar(props) {
               <li><Link to={'/#about--section'} className={props.lightMode ? 'menu-light' : 'menu-dark'}>About</Link></li>
               <li><Link to={'/#projects--section'} className={props.lightMode ? 'menu-light' : 'menu-dark'}>Projects</Link></li>
               <li><Link to={props.lightMode ? '/#contact--section' : '/#contact--section-dark'} className={props.lightMode ? 'menu-light' : 'menu-dark'}>Contact</Link></li>
-              <li><a className={props.lightMode ? 'menu-light' : 'menu-dark'} href={resume} target='_blank'>Resume</a></li>
+              <li><a className={props.lightMode ? 'menu-light' : 'menu-dark'} href={resume} target='_blank'>Resume</a><img className='new-window-icon2' alt='' src={props.lightMode ? newWindow2 : newWindowWhite2} /></li>
             </ul>                       
             <button id={props.lightMode ? 'mode--button-desktop' : 'mode--button-desktop-dark'} className='access--button-desktop' onClick={props.toggleLightMode}><img className='icon' src={props.lightMode ? modeIcon : modeIconWhite}alt='' />{props.lightMode ? " Dark Mode" : " Light Mode"}</button>
             <button id={props.lightMode ? 'animate--button-desktop' : 'animate--button-desktop-dark'} className='access--button-desktop' onClick={props.toggleAnimations}><img src={props.lightMode ? motionIcon : motionIconWhite} className='icon' alt=''/>{props.animationsOn === true ? "Animations OFF" : "Animations ON"}</button>           
