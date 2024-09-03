48c4f78a338fc58810ccc0b5e9dee74971ab1dc7
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-09-03 13:52:29 -0600

    edit about me copy

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -9,20 +9,20 @@ export default function About(props) {
         <h2>About Me</h2>        
         <h4 id={props.lightMode ? 'h4-light' : 'h4-dark'}>A Diverse Background</h4>
         <p>
-        Former Mechanical Engineer, Former Lean Manufacturing Leader, Former Canine Behaviorist, Former Life Coach, Former Implementation Specialist
+        Mechanical Engineer {'>'} Lean Manufacturing Leader {'>'} Canine Behaviorist {'>'} Life Coach {'>'} Implementation Specialist
         <br/><br/>
         I've had quite the career journey!
         <br/><br/>
         The through-line of it all is: <b>Solving Problems and Helping People.</b>
         <br/><br/>
-        I've always been keen to figure out how things work - whether that's assembly lines, animals, or people. This has manifested in a combination of technical professions and those rooted in behavior science. And a strong emphasis on teaching.
+        I've always been keen to figure out how things work - whether that's assembly lines, animals, or people. This has manifested in a combination of technical professions and those rooted in behavior science, with a strong emphasis on teaching.
         </p>      
       
         <h4 id={props.lightMode ? 'h4-light' : 'h4-dark'}>Let's Get Programming!</h4>
         <p>
-        My first exposure to programming was a C++ course for my Mechanical Engineering degree. Later, I loved the challenge of building and updating my own websites for my small businesses using Wix. 
+        My first exposure to programming was a C++ course for my Mechanical Engineering degree. Later, I loved building and updating my own websites for my small businesses using Wix. 
         <br/><br/>
-        Now, I've spent the last 3 years learning Software Engineering through my own grit and determination because it combines all of my interests. <b>Once and engineer, always an engineer.</b> I'm invested in this career for the long term, and I'm bringing all of my experiences and learnings with me as unique strengths!
+        Now, I've spent the last 3 years learning Software Engineering through my own grit and determination. <b>Once and engineer, always an engineer.</b> I'm invested in this career for the long term, and I'm bringing all of my experiences and learnings with me as unique strengths!
         </p>
                 
         <a id={props.lightMode ? 'resume-button-light' : 'resume-button-dark'} href={resume} target='_blank'>View Resume</a>
