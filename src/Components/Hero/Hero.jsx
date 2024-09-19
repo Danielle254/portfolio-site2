1c8cdcbf8cb95690e9fbaae1fa927e2ff582fcb3
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-09-19 09:02:17 -0600

    bug fix: hero button scroll to projects section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './hero.css'
 import graphic from '../../Images/access-image.png'
+import { HashLink as Link } from 'react-router-hash-link'
 
 export default function Hero(props) {
   let animateHero = props.animationsOn ? "animateHero" : "";
@@ -11,7 +12,7 @@ export default function Hero(props) {
         <div id='hero--content'>
           <h1>Hi, I'm Danielle</h1>
           <p>a <span id={props.lightMode ? 'span-light' : 'span-dark'}>Frontend Engineer</span> creating impactful web experiences with a focus on accessibility</p>
-          <a href="/#/#projects--section" id={props.lightMode ? 'hero--button' : 'hero--button-dark'}>View Projects</a>
+          <Link to="/#projects--section" id={props.lightMode ? 'hero--button' : 'hero--button-dark'}>View Projects</Link>
         </div>
         <img src={graphic} className={`graphic ${animateHero}`} alt='web accessibility symbol illustration' />
       </div>
