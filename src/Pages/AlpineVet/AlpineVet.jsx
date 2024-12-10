7febb5879faec7fa894ef00d1a7b6439edf34efe
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-12-10 11:10:00 -0700

    fix: remove unneeded reference to Link

--- src/Pages/AlpineVet/AlpineVet.jsx
+++ src/Pages/AlpineVet/AlpineVet.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useOutletContext } from 'react-router-dom'
-import { Link } from 'react-router-dom'
 import './alpinevet.css'
 
 const AlpineVet = () => {
--- src/Pages/DogFoodCalculator/DogFoodCalculator.jsx
+++ src/Pages/DogFoodCalculator/DogFoodCalculator.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useOutletContext } from 'react-router-dom'
-import { Link } from 'react-router-dom'
 import './dogfoodcalculator.css'
 
 const DogFoodCalculator = () => {
