2a4e00f910f5a001fa08ccd9d69cda65b2e73945
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-12-10 10:01:42 -0700

    feat: add expand/collapse additional projects section

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import { React, useState } from 'react'
 import Project from '../Project/Project'
 import './projects.css'
 
 
+
 export default function Projects(props) {
-  
+
+  const [expandList, setExpandList] = useState(false);
+
   return (
     <section id='projects--section'>
       <h2>Projects</h2>
+      <Project
+        id={5}
+        title='Service Dogs Around Town'
+        highlight={true}
+        summary="This is Version 1 of my capstone project, aimed at helping service dog handlers record information about the businesses they frequent as it relates to the comfort and safety of their dogs."
+        tags={["React", "CSS", "Local Storage", "Forms"]}
+        lightMode={props.lightMode}
+      />
+      <Project
+        id={6}
+        title="Gridiron Survivor"
+        summary="This website displays the agenda for a conference and allows the user to add activities from the official agenda to their own personal itinerary, which they can print as a nicely formatted document. If they need to make changes later, the browser stores their itinerary for them."
+        tags={["React", "CSS", "Local Storage", "Format PDF"]}
+        lightMode={props.lightMode}
+      />
+      {!expandList && <button className={props.lightMode ? 'expand-projects-button' : 'expand-projects-button-dark'} onClick={() => setExpandList(true)}>See More Projects &#x25BC;</button>}
+      {expandList &&
+      <>
       <Project
         id={1}
         title='Service Dogs Around Town'
@@ -37,7 +58,9 @@ export default function Projects(props) {
         tags={["HTML", "CSS", "Accessibility"]}
         lightMode={props.lightMode}
       />
-      <a id={props.lightMode ? 'more-projects-button' : 'more-projects-button-dark'} href="https://github.com/Danielle254" target="_blank">View GitHub</a>
+      <button className={props.lightMode ? 'expand-projects-button' : 'expand-projects-button-dark'} onClick={() => setExpandList(false)}>Hide More Projects &#x25B2;</button>
+      </>}      
+      <a className={props.lightMode ? 'more-projects-button' : 'more-projects-button-dark'} href="https://github.com/Danielle254" target="_blank">View GitHub</a>
     </section>
   )
 }
