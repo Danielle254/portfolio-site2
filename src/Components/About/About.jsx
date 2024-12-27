491640c8d39239a84a47ebf53369766824bae4e9
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-12-27 08:55:13 -0700

    fix: update copy in about section

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -11,20 +11,20 @@ export default function About(props) {
         <div className="about--content-container"> 
           <img alt='front view of Danielle Lindblom sitting at a table in a cafe' className={props.lightMode ? 'danielle-pic' : 'danielle-pic-dark'} src={daniellePic} />
           <div>   
+            <h4 id={props.lightMode ? 'h4-light' : 'h4-dark'}>Once an engineer, always an engineer!</h4>
+            <p>
+            My first exposure to programming was a C++ course for my Mechanical Engineering degree. Later, I built and maintained Wix websites for my small businesses. 
+            <br/><br/>
+            Now, I've spent the last 3 years learning Software Engineering through my own grit and determination. I'm invested in this career for the long term, and I have a particular passion for building accessibility into products from the start.
+            </p>
+
             <h4 id={props.lightMode ? 'h4-light' : 'h4-dark'}>A Diverse Background</h4>
             <p>
             Lean Manufacturing {'>'} Canine Behaviorist {'>'} Life Coach {'>'} SaaS Implementation
             <br/><br/>
             I've had quite the career journey! There's a thru-line, though: <b>Solving Problems and Helping People.</b>
             <br/><br/>
-            I've always been keen to figure out how things work - whether that's machines, animals, or people. This has fueled my passions for engineering and behavior science, with a strong emphasis on teaching.
-            </p>      
-          
-            <h4 id={props.lightMode ? 'h4-light' : 'h4-dark'}>Let's Get Programming!</h4>
-            <p>
-            My first exposure to programming was a C++ course for my Mechanical Engineering degree. Later, I built and updated my own Wix websites for my small businesses. 
-            <br/><br/>
-            Now, I've spent the last 3 years learning Software Engineering through my own grit and determination. <b>Once an engineer, always an engineer.</b> I'm invested in this career for the long term, and I'm bringing all of my experiences and learnings with me as unique strengths!
+            I've always been keen to figure out how things work - whether that's machines, animals, or people. This has fueled my passions for engineering and behavior science, with a strong emphasis on teaching and mentoring.
             </p>
           </div>
         </div>        
